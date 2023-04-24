00eae6cc788791e7d3555e97e0eaa21dc3ed04af
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-24 19:51:11 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Solicite a idade do usuário. Avalie se ele pode ser doador de sangue (i.e. possui idade entre 18 e 67 anos). Informe o resultado da avaliação.
+Console.WriteLine("--- Doador de Sangue ---");
+
+Console.Write("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine()!);
+
+if (idade >= 18 && idade <= 67) {
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Você pode ser um doador de sangue.");
+
+} else {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você não pode ser um doador de sangue.");
+}
+Console.ResetColor();
